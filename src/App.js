b08b1494a716b54c9fca1aa852f07a4c8ad14775
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2025-07-26 00:40:19 +0530

    describe, onchange - Jest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [data,setData] =useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>First React test case</p>
+     <input type='text' value={data} onChange={(e)=>setData(e.target.value + "test")} placeholder='Enter username' />
+     <br />
+     <br />
+     <img title='AI Genrated Image' src='https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg3919321_1443393332_n.jpg'
+     />
+
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("UI test case group", () => {
+
+  test("Test first React app case", () => {
+    render(<App />);
+    let text = screen.getByText(/First React test Case/i);
+    const title = screen.getByTitle("AI Genrated Image");
+    expect(text).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+
+  })
+
+  test("Testing input box", () => {
+    render(<App />);
+    let checkInput = screen.getByRole("textbox");
+    let checkInputPlaceholder = screen.getByPlaceholderText("Enter username");
+    expect(checkInput).toBeInTheDocument();
+    expect(checkInputPlaceholder).toBeInTheDocument();
+  })
+
+})
+
+describe("API test case group", () => {
+
+  test("API Test first React app case", () => {
+    render(<App />);
+    let text = screen.getByText(/First React test Case/i);
+    const title = screen.getByTitle("AI Genrated Image");
+    expect(text).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+
+  })
+
+  describe("Nested decribe",()=>{
+    test("API Testing input box", () => {
+      render(<App />);
+      let checkInput = screen.getByRole("textbox");
+      let checkInputPlaceholder = screen.getByPlaceholderText("Enter username");
+      expect(checkInput).toBeInTheDocument();
+      expect(checkInputPlaceholder).toBeInTheDocument();
+    })
+  })
+
+})
+
+describe("On change event testing",()=>{
+
+  test("Onchange event testing",()=>{
+    render(<App />)
+    let input = screen.getByRole("textbox");
+    fireEvent.change(input,{target:{value:'a'}});
+    expect(input.value).toBe("atest");
+  })
+
+})
